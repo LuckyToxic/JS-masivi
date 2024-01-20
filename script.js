93e466852849f7bbb9e601415a5dc7c6959d3802
LuckyToxic <mrbalumba27@gmail.com>
2024-01-20 16:15:35 +0300

    new

---
+++ script.js
@@ -0,0 +1,66 @@
+// let arr = [ 1,2,3,4,5]    //Первое задание 
+
+// function result(){
+//     console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4])
+// }
+
+// result()
+
+
+
+
+// let numArr = [1,2,3,4,5,6,7,8,9,10]     //Третье задание 
+
+// for( let i = 0; i < numArr.length;i++){
+//     if (numArr[i] % 2 == 0){
+//         console.log(numArr[i])
+//     }
+// }
+
+
+
+
+
+// let arr2 = [1,5,6,4,8,3]     //Шестое задание 
+
+// function delEle(x){
+//     arr2.splice(x,1)
+// }
+
+// delEle(0)
+
+// console.log(arr2)
+
+
+
+
+// let fruits = ['Яблоко','Апельсин','Груша']   //Четвортое задание
+
+// function moreFruits(x,y){
+//     fruits.push(x,y)
+//     console.log(fruits)
+// }
+
+// moreFruits('Банан','Кокос')
+
+
+
+
+// let numArr2 = [1,2,3,4,5,6,7,8,9,10]      //Восемьнадцатое задание
+
+// let newNum = []
+
+// function lz(){ 
+//     for( let i = 0; i < numArr2.length;i++){
+//         if(numArr2[i] % 2 == 0){
+//             newNum.push(numArr2[i])
+//         }
+//     }
+// }
+
+
+// lz()
+
+// console.log(newNum)
+
+// console.log(numArr2)
